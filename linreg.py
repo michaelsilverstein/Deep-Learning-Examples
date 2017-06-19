18ee3fdb9645f743cb14da6a454dd1413282c753
michaelsilverstein <msilver4@bu.edu>
2017-06-19 08:38:55 -0400

    Functions from jupyter notebook

--- linreg.py
+++ linreg.py
@@ -1,57 +1,76 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib
+import itertools
 
 #Deep Learning
 #http://www.deeplearningbook.org/
 #Figure 5.1
 #Linear regression
 
-# y_hat = w.T x + b
-# w = (X.T X)^-1 X.T y
-def MSE(y_,y):
-    #Mean square error
-    return 1./len(y)*np.power(np.linalg.norm(y_-y),2)
-def plots():
-    ##Plot
-    fig, axs = plt.subplots(ncols=2)
+matplotlib.rc('text', usetex=True)
+matplotlib.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"] #For Latex later on
 
-    # Linear regression
-    axs[0].plot(x[:, 0], y, 'o', label='Actual')
-    axs[0].plot(x[:, 0], y_hat, label='Predicted')
-    axs[0].legend()
-    axs[0].set_xlabel(r'$x_1$')
-    axs[0].set_ylabel(r'$y$')
+def datagen(dim,size):
+    #Input:
+    #|dim = number of features
+    #|size = sample size
+    #Output:
+    #x: features in dimensions `dim` of size `size`
+    #y: observations of size `size
+    jitter = 0.3 #Spread parameter of noise
+    seed = np.array([np.random.rand(size)]*dim) #Linear relationship in dim dimensions
+    noise = np.random.rand(dim,size) * jitter
+    x = np.vstack([seed+noise,np.ones(size)]).T #append column of ones for bias (y-intercept)
+    y = seed[0] #Vector of observations
+    return x,y
+def MSE(y_,y,size):
+    #Compute MSE for a vector of predictions y_ and observations y
+    return 1./size*np.power(np.linalg.norm(y_-y),2)
 
-    # Comparing ws
-    axs[1].plot(ws[:,0], MSEs, 'black')
-    axs[1].plot(w[0], MSE_, 'o')
-    axs[1].set_xlabel(r'$w_1$')
-    axs[1].set_ylabel(r'$MSE^{(train)}$')
 
-    fig.suptitle('Figure 5.1')
-    plt.show()
-    # plt.savefig('figures/fig5.1.png')
-"""MAIN"""
-size = 100
-dim=1
-jitter = 0.3 #Spread parameter
-x = np.array([np.concatenate([r,[1]]) for r in np.random.rand(size,dim)])
-y = x[:,0]+(np.random.rand(size)*jitter) #jittery but linearish
-
-##Calculate best w that minimizes MSE
-w = np.dot(
+def linreg(x, y):
+    # Train model parameters, w, based on matrix of features,x, and observations, y
+    w = np.dot(
         np.dot(
             np.linalg.inv(
-                np.dot(x.T,x)),
-                    x.T),
-                        y)
-y_hat = np.dot(x,w)#Dot product
+                np.dot(x.T, x)),
+            x.T),
+        y)
+    return w
 
-#Explore MSE landscape for w1 for 1 feature case
-if dim == 1:
-    ws = np.array([[x_,w[1]] for x_ in np.linspace(0,w[0]+1,20)]) #From 0 to a bit past the solution, keeping y-int constant
-    y_hats = np.array([np.dot(x,w_) for w_ in ws]) #Predicted y for each w
-    MSEs = [MSE(y_,y) for y_ in y_hats] #MSE for all ws
-    MSE_ = MSE(y_hat,y) #For the best w, calculated in the prev step
 
-plots()
+def gridsearch(x,y,size, ax=False, amin=-.5, amax=1, contour=False):
+    # Find optimal parameters through a grid search with resolution sizeXsize
+    # Input:
+    # |x: feature matrix
+    # |y: observation vector
+    # |size: step size between points in grid; grid resolution
+    # |ax: When plot is desired, pass ax object to plot
+    # |amin,amax = axis min and max
+    # |contour: plot contour plot instead of scatter
+    # Output:
+    # |w: parameters that minimize MSE
+
+    # Generate mesh
+    seed = np.linspace(amin, amax, size)
+    slope, b = np.meshgrid(seed, seed)  # 50x50 grid from 0 to 2
+    combos = np.vstack([slope.ravel(), b.ravel()])  # All pairwise combinations of parameters in grid
+
+    # Compute y_hat for each set of parameters
+    y_tests = np.dot(x, combos).T  # y_tests = [y_hats{1},...,y_hats{size}], for pairs of parameters 1,...,size
+    MSEs = np.array([MSE(y_, y) for y_ in y_tests]).reshape(size, size)  # Calculate MSE for each set of y_hats
+    minval_ix = np.unravel_index(np.argmin(MSEs), (size, size))  # Find index at which MSE is the smallest
+
+    w = [seed[minval_ix[1]], seed[minval_ix[0]]]
+
+    if ax:
+        if contour:
+            ax.contourf(slope, b, MSEs)
+        else:
+            ax.scatter(slope, b, c=MSEs)
+        ax.set_xlabel('slope')
+        ax.set_ylabel('b')
+    return w
